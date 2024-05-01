29526e184b47de0679781e9402b5003d5c3c50f4
goldstargloww <goldstargloww@gmail.com>
2024-04-30 21:11:46 -0400

    made the tabbing consistent

--- public/bars.js
+++ public/bars.js
@@ -1,60 +1,60 @@
 class Header extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
   
     connectedCallback() {
-      this.innerHTML = 
-              `<div id="headerArea">
-                  <div id="header"><img src="https://felicitations.neocities.org/header.png" alt=""></div>
+        this.innerHTML = 
+            `<div id="headerArea">
+                <div id="header"><img src="https://felicitations.neocities.org/header.png" alt=""></div>
                   
-              <nav id="navbar">
-                <ul class="vcr">
-                  <li><img src="https://felicitations.neocities.org/Craft.png" alt="" width="32" class="head"></li>
-                  <li><a href="https://felicitations.neocities.org/index.html">Home</a></li>
-                  <li><a href="https://felicitations.neocities.org/overview/events.html">Special Events</a></li>
-                  <li><a href="https://felicitations.neocities.org/overview/acts.html">Story Events</a></li>
-                  <li><a href="https://felicitations.neocities.org/overview/random">Random Events</a></li>
-                  <li><a href="https://felicitations.neocities.org/overview/rooms.html">Room Dialogues</a></li>
-                  <li><a href="https://felicitations.neocities.org/thanks">Special Thanks</a></li>
-                  <li><img src="https://felicitations.neocities.org/Craft.png" alt="" width="32" class="head"></li>
-                </ul>
+                <nav id="navbar">
+                    <ul class="vcr">
+                        <li><img src="https://felicitations.neocities.org/Craft.png" alt="" width="32" class="head"></li>
+                        <li><a href="https://felicitations.neocities.org/index.html">Home</a></li>
+                        <li><a href="https://felicitations.neocities.org/overview/events.html">Special Events</a></li>
+                        <li><a href="https://felicitations.neocities.org/overview/acts.html">Story Events</a></li>
+                        <li><a href="https://felicitations.neocities.org/overview/random">Random Events</a></li>
+                        <li><a href="https://felicitations.neocities.org/overview/rooms.html">Room Dialogues</a></li>
+                        <li><a href="https://felicitations.neocities.org/thanks">Special Thanks</a></li>
+                        <li><img src="https://felicitations.neocities.org/Craft.png" alt="" width="32" class="head"></li>
+                    </ul>
                 </nav>
 
-                 <div id="flex">
-                  <aside id="leftSidebar">
-                    <h2>Font Changers</h2>
-                    <button onclick="changeFontStyle(null)">Default</button>
-                    <button onclick="changeFontStyle('VCR_OSD_MONO')">ISAT Font</button>
-                    <button onclick="changeFontStyle('OpenDyslexic3')">OpenDyslexic3</button>
+                <div id="flex">
+                    <aside id="leftSidebar">
+                        <h2>Font Changers</h2>
+                        <button onclick="changeFontStyle(null)">Default</button>
+                        <button onclick="changeFontStyle('VCR_OSD_MONO')">ISAT Font</button>
+                        <button onclick="changeFontStyle('OpenDyslexic3')">OpenDyslexic3</button>
 
-                    <h2>The ISAT script</h2>
+                        <h2>The ISAT script</h2>
                         <ul>
-                          <li><a href="https://felicitations.neocities.org/about.html">About</a></li>
-                          <li><a href="https://felicitations.neocities.org/feed.txt">RSS Feed</a></li>
-                          <li><a href="https://felicitations.neocities.org/portraits">Dialogue portrait names</a></li>
-                          <li>For questions and requests, reach out on <a href="https://isat-script-project.tumblr.com/">Tumblr.</a></li>
+                            <li><a href="https://felicitations.neocities.org/about.html">About</a></li>
+                            <li><a href="https://felicitations.neocities.org/feed.txt">RSS Feed</a></li>
+                            <li><a href="https://felicitations.neocities.org/portraits">Dialogue portrait names</a></li>
+                            <li>For questions and requests, reach out on <a href="https://isat-script-project.tumblr.com/">Tumblr.</a></li>
                         </ul>
-                </aside>
-                </div>`
-      ;
+                    </aside>
+                </div>
+            </div>`
+        ;
     }
-  }
+}
   
-  customElements.define('head-er', Header);
+customElements.define('head-er', Header);
   
-  class Footer extends HTMLElement {
+class Footer extends HTMLElement {
     constructor() {
-      super();
+        super();
     }
   
     connectedCallback() {
-      this.innerHTML = `<footer id="footer">The script is free to use even without attribution to me. <b>In Stars and Time</b> belongs to insertdisc5.</footer>`
-      ;
+        this.innerHTML = `<footer id="footer">The script is free to use even without attribution to me. <b>In Stars and Time</b> belongs to insertdisc5.</footer>`;
     }
-  }
+}
   
-  customElements.define('foot-er', Footer);
+customElements.define('foot-er', Footer);
 
 
 // for questions, ask riu, cuz she made all of this, lmao. I have no idea how it works. -Feli //
@@ -68,27 +68,27 @@ class Header extends HTMLElement {
 
 
 function changeFontStyle(fontFamily) {
-  var change;
-  var elements = document.querySelectorAll('body,body *:not(script)');
-      
-  var isSetOrNull = elements[0].style.fontFamily == fontFamily || !fontFamily || fontFamily == 'null';
-      
-  if(!isSetOrNull) {
-    sessionStorage.setItem('font', fontFamily);
-    change = (element) => element.style.fontFamily = fontFamily;
-  }
-  else {
-    sessionStorage.removeItem('font');
-    change = (element) => element.style.fontFamily = null;
-  }
-  
-  elements.forEach(element => change(element));
+    var change;
+    var elements = document.querySelectorAll('body,body *:not(script)');
+
+    var isSetOrNull = elements[0].style.fontFamily == fontFamily || !fontFamily || fontFamily == 'null';
+
+    if(!isSetOrNull) {
+        sessionStorage.setItem('font', fontFamily);
+        change = (element) => element.style.fontFamily = fontFamily;
+    }
+    else {
+        sessionStorage.removeItem('font');
+        change = (element) => element.style.fontFamily = null;
+    }
+
+    elements.forEach(element => change(element));
 }
 
 // load correct font if it had been set previously
 window.onload = function() {
-  var font = sessionStorage.getItem('font');
-  if(!!font) {
-    changeFontStyle(font);
-  }
+    var font = sessionStorage.getItem('font');
+    if(!!font) {
+        changeFontStyle(font);
+    }
 }
