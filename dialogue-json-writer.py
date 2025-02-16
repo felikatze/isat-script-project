abe3ba70f2b7d3f249e97c57c58c68e0771f1158
goldstargloww <goldstargloww@gmail.com>
2025-02-16 00:06:53 -0500

    small changes for code readability

--- dialogue-json-writer.py
+++ dialogue-json-writer.py
@@ -3,56 +3,70 @@ import json
 import os.path
 
 
-allPublicSubdirectoryPageURIs = [os.path.join(dirpath,f) for (dirpath, dirnames, filenames) in os.walk("public") for f in filenames]
+all_public_subdirectory_page_URIs = [os.path.join(dirpath,f) for (dirpath, dirnames, filenames) in os.walk("public") for f in filenames]
+
 parsed_dialogue_lines = []
-for uri in allPublicSubdirectoryPageURIs:
 
-    if uri.split('.')[1] != "html":
-        continue
+for uri in all_public_subdirectory_page_URIs:
+
+    if uri.split('.')[-1] != "html":
+        continue # skip if it's not an html file
     print(uri)
-    file = open(uri, "r", encoding="utf8")
-    parsed_html = bs4.BeautifulSoup(file.read(), features="html.parser")
-    pageTitle = parsed_html.find('title').text
-    print(pageTitle)
+    
+    with open(uri, "r", encoding="utf8") as file:
+        parsed_html = bs4.BeautifulSoup(file.read(), features="html.parser")
+    
+    page_title = parsed_html.find('title').text
+    print(page_title)
+    
     dialogue_heads = parsed_html.body.findAll('span', attrs={'class', 'dialogue-head'})
-
     dialogue_lines = parsed_html.body.findAll(True, attrs={'class', 'dialogue-line', 'dialogue-option-only'})
     dialogue_lines_linebreak_parsed = []
 
     for br in parsed_html.body.findAll('br'):
         br.replace
-
         
     for br in parsed_html.body.findAll('br'):
-        br.replaceWith("\n")
+        br.replaceWith("\n") # replace <br> with \n
 
     for line in dialogue_lines:
-        lineParser = bs4.BeautifulSoup(str(line), features="html.parser")
+        line_parser = bs4.BeautifulSoup(str(line), features="html.parser")
 
-        possibleExpression = lineParser.find(True, attrs=('class', 'dialogue-expression'))
-        expression = "" if possibleExpression is None else possibleExpression.text
+        # get expression, if it exists
+        possibleExpression = line_parser.find(True, attrs=('class', 'dialogue-expression'))
+        expression = possibleExpression.text if possibleExpression else ""
 
-        possibleSpeaker = lineParser.find(True, attrs=('class', 'dialogue-name'))
-        speaker = "" if possibleSpeaker is None else possibleSpeaker.text
+        # get speaker, if it exists
+        possibleSpeaker = line_parser.find(True, attrs=('class', 'dialogue-name'))
+        speaker = possibleSpeaker.text if possibleSpeaker else ""
 
-        for dh in lineParser.findAll('span', attrs={'class', 'dialogue-head', 'dialogue-name'}):
-            dh.decompose()
-        line = lineParser.text
-        if(len(line.strip()) == 0):
-            continue
-        if(not "\n" in line):
+        for dialogue_head in line_parser.findAll('span', attrs={'class', 'dialogue-head', 'dialogue-name'}):
+            dialogue_head.decompose() # delete all dialogue heads; don't need them anymore
+        
+        line = line_parser.text
+        if not line.strip(): # if the string is just whitespace
+            continue # skip to next line
+        
+        if "\n" not in line:
             dialogue_lines_linebreak_parsed.append([line.strip(), expression, speaker])
             continue
-        splitLines = str(line.strip()).split("\n")
-        for sl in splitLines:
-            dialogue_lines_linebreak_parsed.append([sl, expression, speaker])
+        
+        split_lines = line.strip().split("\n")
+        for split_line in split_lines:
+            dialogue_lines_linebreak_parsed.append([split_line, expression, speaker])
 
     past_lines = []
     for line in dialogue_lines_linebreak_parsed:
-        element_number = 1
         element_number = sum(line in s for s in past_lines) + 1
         past_lines.append(line)
-        parsed_dialogue_lines.append([uri.removeprefix("public"), line[0], pageTitle, element_number, line[1], line[2]])
+        parsed_dialogue_lines.append([
+            uri.removeprefix("public"),
+            line[0],
+            page_title,
+            element_number,
+            line[1],
+            line[2]
+        ])
 
         
 
--- public/search.js
+++ public/search.js
@@ -1,46 +1,48 @@
-
 // Code for the searching and highlighting of the requested text in the URL
+
 var currentPath = window.location.pathname.replaceAll("/", "\\")
-window.addEventListener("load", (event)=> {
-var match = window.location.hash.match(/^#!s(-?\d*)!(.*)$/);
-if (match && match[2]) {
 
+window.addEventListener("load", () => {
+    var match = window.location.hash.match(/^#!s(-?\d*)!(.*)$/);
 
-// Looks for a detailTag whose children contains the uriSearch. Opens it if it exists. 
-var detailTags = document.getElementsByTagName("details");
-var uriSearch = decodeURI(match[2].replaceAll("%20", " "));
-for (var i = 0; i < detailTags.length; i++) {
-  for(var j = 0; j < detailTags[i].childNodes.length; j++){
-    if (detailTags[i].childNodes[j].textContent.includes(uriSearch)) {
-        detailTags[i].setAttribute('open', '')
-        break;
+    if (match && match[2]) {
+        // Looks for a detailTag whose children contains the uriSearch. Opens it if it exists.
+
+        var detailTags = document.getElementsByTagName("details");
+        var uriSearch = decodeURI(match[2].replaceAll("%20", " "));
+
+        for (var i = 0; i < detailTags.length; i++) {
+            for (var j = 0; j < detailTags[i].childNodes.length; j++) {
+                if (detailTags[i].childNodes[j].textContent.includes(uriSearch)) {
+                    detailTags[i].setAttribute('open', '');
+                    break
+                }
+            }
+        }
+
+        // Parses the uri for search parameters, then highlights the correct text based on those parameters
+        var count = match[1] || 1;
+        var backwards = false;
+        if (count < 0) {
+            backwards = true;
+            count = Math.abs(count);
+        }
+
+        for (var i = 0; i < count; i++) {
+            window.find(decodeURI(match[2]), false, backwards, false, false, true, false);
+        }
     }
-  }
-}
-
-// Parses the uri for search parameters, then highlights the correct text based on those parameters
-  var count = match[1] || 1;
-  var backwards = false;
-  if (count < 0) {
-    backwards = true;
-    count = Math.abs(count);
-  }
-  
-  for (var i = 0; i < count; i++) {
-    window.find(decodeURI(match[2]), false, backwards, false, false, true, false);
-  }
-}
 })
-// Hard page refresh function, used to force a new search on search results that redirect to the current page
+
 function RefreshPage(queryString) {
+    // Hard page refresh function, used to force a new search on search results that redirect to the current page
     var newUrl = window.location.origin + window.location.pathname + "?key=" + queryString;
     window.location.href = newUrl;
     return false;
 }
-//-----------------------------------------------------------------
 
-// Defining Search-er element, which contains the searchbar, results list and its items
 class Searcher extends HTMLElement {
+    // Defining Search-er element, which contains the searchbar, results list and its items
     constructor() {
         super();
     }
@@ -55,11 +57,10 @@ class Searcher extends HTMLElement {
     }
 }
 customElements.define("search-er", Searcher);
-// -----------------------------------------------------------------------
- 
 
- let searchterm = "";
- function filterLines(lines){
+
+let searchterm = "";
+function filterLines(lines) {
     searchterm = searchbox.value;
     filteredLines = lines.filter(x => x[1].toLowerCase().includes(searchterm.toLowerCase()));
     return filteredLines
@@ -69,24 +70,27 @@ var searchbox = document.getElementById("searchbox");
 var resultsList = document.getElementById("searchUL");
 
 searchbox.onkeyup = function() {
-  let filteredLines = filterLines(allLines)
-  modifyResultsList(filteredLines)
-  if(filteredLines.length == 0 && !resultsList.classList.contains("hidden"))
-    toggleElementVisibility(resultsList);
+    let filteredLines = filterLines(allLines);
+    modifyResultsList(filteredLines);
+    if (filteredLines.length == 0 && !resultsList.classList.contains("hidden")) {
+        toggleElementVisibility(resultsList)
+    }
 }
 
 async function getAllDialogueLines() {
     return fetch("/dialogue-lines.json")
-     .then((response) => response.text())
-     .then((text) => {
-       return JSON.parse(text);
-     });
- }
+        .then((response) => response.text())
+        .then((text) => {
+            return JSON.parse(text)
+        }
+    )
+}
 
- let allLines = []
- searchbox.onclick = async function() {
-    if(allLines.length == 0)
-        allLines = await getAllDialogueLines();
+let allLines = []
+searchbox.onclick = async function() {
+    if (allLines.length == 0) {
+        allLines = await getAllDialogueLines()
+    }
     toggleElementVisibility(resultsList);
     console.log(allLines)
 }
@@ -96,73 +100,76 @@ async function getAllDialogueLines() {
 
 document.addEventListener('click', (e) => {
     if ((e.currentTarget.activeElement.id !== "searchbox") && !resultsList.classList.contains("hidden")) {
-        toggleElementVisibility(resultsList);
+        toggleElementVisibility(resultsList)
     }
 })
 
 function toggleElementVisibility(el) {
     if (!el.classList.contains("hidden")) {
-        el.classList.add("hidden");
+        el.classList.add("hidden")
     }
     else {
-        el.classList.remove("hidden");
+        el.classList.remove("hidden")
     }
 }
 
 function modifyResultsList(lines) {
     console.log("heyoo")
     console.log(lines)
-    if (lines.length == 0 || searchterm.length <= 3)
-        clearSearchResults();
-    
-        if (lines.length < 100)
-            setSearchResults(lines)
-        
-        else if(searchterm.length > 3)
-            setSearchResults(lines.slice(0, 100))
+
+    if (lines.length == 0 || searchterm.length <= 3) {
+        clearSearchResults()
+    }
+
+    if (lines.length < 100) {
+        setSearchResults(lines)
+    }
+    else if (searchterm.length > 3) {
+        setSearchResults(lines.slice(0, 100))
+    }
 }
 
-function createSearchResultItem(uri, text, source, nth_instance){
-    let resultItem = document.createElement('li')
-    let linkItem = document.createElement('a')
-    let textnode = document.createTextNode(text)
-    let lineBreakItem = document.createElement('br')
-    let sourceItem = document.createElement('p')
-    let sourceTextNode = document.createTextNode("(In: " + source + ")")
+function createSearchResultItem(uri, text, source, nth_instance) {
+    let resultItem = document.createElement('li');
+    let linkItem = document.createElement('a');
+    let textnode = document.createTextNode(text);
+    let lineBreakItem = document.createElement('br');
+    let sourceItem = document.createElement('p');
+    let sourceTextNode = document.createTextNode("(In: " + source + ")");
 
-    sourceItem.appendChild(sourceTextNode)
-    sourceItem.classList.add('search-result-source')
-    linkItem.appendChild(textnode)
-    linkItem.appendChild(lineBreakItem)
-    linkItem.appendChild(sourceItem)
-    resultItem.appendChild(linkItem)
-    resultItem.classList.add("dialogue")
-    resultItem.classList.add("search-result")
-    linkItem.classList.add("search-result-link")
+    sourceItem.appendChild(sourceTextNode);
+    sourceItem.classList.add('search-result-source');
+    linkItem.appendChild(textnode);
+    linkItem.appendChild(lineBreakItem);
+    linkItem.appendChild(sourceItem);
+    resultItem.appendChild(linkItem);
+    resultItem.classList.add("dialogue");
+    resultItem.classList.add("search-result");
+    linkItem.classList.add("search-result-link");
+
     newUri = uri + "#!s" + nth_instance + "!" + text;
-    if(currentPath == uri)
+    if (currentPath == uri) {
         linkItem.addEventListener("click", function() {
             RefreshPage("#!s" + nth_instance + "!" + text)
-        });
-        
-    linkItem.setAttribute("href", newUri)
+        })
+    }
+    linkItem.setAttribute("href", newUri);
     return resultItem;
 }
 
-function setSearchResults(setListLines){
+function setSearchResults(setListLines) {
     clearSearchResults();
     console.log("setSearchResults")
-    for(let i = 0; i < setListLines.length; i++)
-    {
-        let uri = setListLines[i][0]
-        let text = setListLines[i][1]
-        let source = setListLines[i][2]
-        let nth_instance = setListLines[i][3]
-        resultItem = createSearchResultItem(uri, text, source, nth_instance)
+    for (let i = 0; i < setListLines.length; i++) {
+        let uri = setListLines[i][0];
+        let text = setListLines[i][1];
+        let source = setListLines[i][2];
+        let nth_instance = setListLines[i][3];
+        resultItem = createSearchResultItem(uri, text, source, nth_instance);
         resultsList.appendChild(resultItem)
     }
     if (resultsList.classList.contains("hidden")) {
-        toggleElementVisibility(resultsList);
+        toggleElementVisibility(resultsList)
     }
 }
 
@@ -172,6 +179,6 @@ function clearSearchResults() {
     }
 
     if (!resultsList.classList.contains("hidden")) {
-        toggleElementVisibility(resultsList);
+        toggleElementVisibility(resultsList)
     }
 }
