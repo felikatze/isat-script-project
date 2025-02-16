8cc3264a9989c5c05ca7f2f833de705c24d9490f
goldstargloww <goldstargloww@gmail.com>
2025-02-16 02:13:16 -0500

    half done unnamed function because i need to sleep so far it cleans up (removes formatting things and macros and stuff) all the lines in translations.json and puts them in a list of dicts like {"english": "sdhgjkdfs", "japanese": "hfdgjkdf"}

--- translations.py
+++ translations.py
@@ -1,4 +1,4 @@
-import bs4, json, os
+import bs4, json, os, re
 
 
 def generate_filelist() -> list[str]:
@@ -10,7 +10,6 @@ def generate_filelist() -> list[str]:
                 filelist.append(os.path.join(root, file))
     return filelist
 
-
 def generate_filecontent(filelist: list[str] = generate_filelist()) -> dict[str, bs4.element.ResultSet[bs4.Tag]]:
     print("generating filecontent")
     filecontent = {}
@@ -20,35 +19,59 @@ def generate_filecontent(filelist: list[str] = generate_filelist()) -> dict[str,
             filecontent[file] = soup.select(".dialogue-line")
     return filecontent
         
-def write_to_file(data):
-    with open("text.txt", "w", encoding="utf-8") as f:
-        f.write(data)
-
-filecontent = generate_filecontent()
-print('doing things')
-pages = {}
-for file in filecontent:
-    print(f"doing {file}")
-    content = filecontent[file]
-    lines = []
-    for stuff in content:
-        line = {}
-        
-        speaker = stuff.select(".dialogue-name")
-        if speaker:
-            line["speaker"] = speaker[0].decode_contents()
+def write_lines_json():
+    filecontent = generate_filecontent()
+    print('doing things')
+    pages = {}
+    for file in filecontent:
+        print(f"doing {file}")
+        content = filecontent[file]
+        lines = []
+        for stuff in content:
+            line = {}
             
-        head = stuff.select(".dialogue-head")
-        if head:
-            for element in head:
-                element.decompose()
-        dialogue = stuff.decode_contents(indent_level=0).removesuffix('\n')
-        if dialogue:
-            line["dialogue"] = dialogue
-        
-        lines.append(line)
-    pages[file] = lines
+            speaker = stuff.select(".dialogue-name")
+            if speaker:
+                line["speaker"] = speaker[0].decode_contents()
+                
+            head = stuff.select(".dialogue-head")
+            if head:
+                for element in head:
+                    element.decompose()
+            dialogue = stuff.decode_contents(indent_level=0).removesuffix('\n')
+            if dialogue:
+                line["dialogue"] = dialogue
+            
+            lines.append(line)
+        filepath = file.removeprefix("public\\").replace("\\", "/")
+        pages[filepath] = lines
 
-print("writing")
-with open("lines.json", "w", encoding="utf-8") as outfile:
-    json.dump(pages, outfile, indent=4)
+    print("writing")
+
+    with open("lines.json", "w", encoding="utf-8") as outfile:
+        json.dump(pages, outfile, indent=4)
+        
+
+
+def name_this_later():
+    with open("Translations.json", encoding="utf-8") as file:
+        game_translations_file: dict = json.load(file)
+        
+    with open("lines.json", encoding="utf-8") as file:
+        site_lines_file: dict = json.load(file)
+
+    pattern = re.compile(r"\\(m\[(v([smioblhkq](np)?|(wo)?man||nb|kid|sc|tuto|cg)|clear|wait|dot|rb|choice(var|space)|wish(on|off))\]|!\\|!|\.|\||\^|>|<|(reset)?shake|wave|{|}|v\[\d+?\]|n<.+?>|fi|i\[\d+?\]|ls(on|off|piv?\[\d+?\]))")
+    
+    
+    game_lines = []
+    i = 0
+    
+    for original_english_line in game_translations_file["msg"]:
+        original_japanese_line = game_translations_file["msg"][original_english_line]["Japanese"]
+        
+        new_english_line = re.sub(pattern, '', original_english_line)
+        new_japanese_line = re.sub(pattern, '', original_japanese_line)
+        
+        line = {"en": new_english_line, "jp": new_japanese_line}
+        game_lines.append(line)
+    
