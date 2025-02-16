2280b03b40177d412bfd0c708b7b37cc18ecf65b
goldstargloww <goldstargloww@gmail.com>
2025-02-15 23:16:48 -0500

    idk what i'm doing but i don't think i like it and might redo the whole thing but i need to switch branches so enjoy

---
+++ translations.py
@@ -0,0 +1,52 @@
+import bs4, json, os
+
+
+def generate_filelist() -> list[str]:
+    print("generating filelist")
+    filelist = []
+    for root, dirs, files in os.walk("public"):
+        for file in files:
+            if file.endswith(".html"):
+                filelist.append(os.path.join(root, file))
+    return filelist
+
+
+def generate_filecontent(filelist: list[str] = generate_filelist()) -> dict[str, bs4.element.ResultSet[bs4.Tag]]:
+    print("generating filecontent")
+    filecontent = {}
+    for file in filelist:
+        with open(file, encoding="utf-8") as f:
+            soup = bs4.BeautifulSoup(f.read(), "html5lib")
+            filecontent[file] = soup.select(".dialogue-line")
+    return filecontent
+        
+def write_to_file(data):
+    with open("text.txt", "w", encoding="utf-8") as f:
+        f.write(str(data))
+
+filecontent = generate_filecontent()
+print('doing things')
+pages = {}
+for file in filecontent:
+    print(f"doing {file}")
+    content = filecontent[file]
+    lines = []
+    for stuff in content:
+        line = {}
+        
+        speaker = stuff.select(".dialogue-name")
+        if speaker:
+            line["speaker"] = speaker[0].decode_contents()
+            
+        head = stuff.select(".dialogue-head")
+        if head:
+            for element in head:
+                element.decompose()
+        dialogue = stuff.decode_contents(indent_level=0).removesuffix('\n')
+        if dialogue:
+            line["dialogue"] = dialogue
+        
+        lines.append(line)
+    pages[file] = lines
+
+write_to_file(pages)
