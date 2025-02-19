79bfa1dcf20df9bf731976dd697476a3a9ee4956
goldstargloww <goldstargloww@gmail.com>
2025-02-18 20:55:30 -0500

    automate some formatting

--- translations.py
+++ translations.py
@@ -121,16 +121,20 @@ def clean_site_lines():
         
     newline_pattern = re.compile(r"(\n )?<br\/>(\n )?|\n ?")
     tag_pattern = re.compile(r"<.+?>")
+    missing_space_pattern = re.compile(r"(\?|(?<!\\)!)\w")
         
-    for page in site_lines_file:
-        if not re.match(ignored_pages, page):
-            for line in site_lines_file[page]:
-                try:
-                    line["dialogue_clean"] = re.sub(newline_pattern, '', line["dialogue"])
-                    line["dialogue_clean"] = re.sub(tag_pattern, '', line["dialogue_clean"])
-                except KeyError:
-                    line["dialogue"] = ""
-                    line["dialogue_clean"] = ""
+    with alive_bar(sum([len(page) for page in site_lines_file])) as bar:
+        for page in site_lines_file:
+            if not re.match(ignored_pages, page):
+                for line in site_lines_file[page]:
+                    try:
+                        line["dialogue_clean"] = re.sub(newline_pattern, '', line["dialogue"])
+                        line["dialogue_clean"] = re.sub(tag_pattern, '', line["dialogue_clean"])
+                        line["dialogue_clean"] = re.sub(missing_space_pattern, '', line["dialogue_clean"])
+                    except KeyError:
+                        line["dialogue"] = ""
+                        line["dialogue_clean"] = ""
+                    bar()
                     
     write_json_to_file("site_lines.json", site_lines_file)
         
@@ -189,4 +193,40 @@ def generate_page_line_associations():
             associate_lines_for_page(file)
             bar()
 
+def format_line_associations(json_path: str | None = None):
+    # can do this manually with ctrl f
+    # shakes:
+    #    "\\\\shake(.+?)("|\\\\resetshake)
+    #    "<span class=\"shake\">$1</span>"
+    # waves:
+    #    "\\\\wave(.+?)("|\\\\resetshake)
+    #    "<span class=\"wave\">$1</span>"
+    shake_pattern = re.compile(r"\\shake(.+?)($|\\resetshake)")
+    wave_pattern = re.compile(r"\\wave(.+?)($|\\resetshake)")
+    unneeded_stuff_pattern = re.compile(r"(\\(m\[(v([smioblhkq](np)?|(wo)?man||nb|kid|sc|tuto|cg)|clear|rb|choice(var|space))\]|!\\|!|n<.+?>|fi|i\[\d+?\]|ls(on|off|piv?\[\d+?\])|bustClear\[\d+?,\d+?\]))", flags=re.IGNORECASE)
+    
+    
+    if not json_path:
+        path = "line_associations"
+        with alive_bar(len(os.listdir(path))) as bar:
+            for file in os.listdir(path):
+                with open(path + "/" + file, encoding="utf-8") as f:
+                    file_lines: list[dict[str, str | None | bool]] = json.load(f)
+                for line in file_lines:
+                    if not line["checked"]:
+                        if type(line["jp_raw"]) == type("string"):
+                            line["jp_raw"] = re.sub(unneeded_stuff_pattern, '', line["jp_raw"])
+                            line["jp_raw"] = re.sub(shake_pattern, r'<span class="shake">\1</span>', line["jp_raw"])
+                            line["jp_raw"] = re.sub(wave_pattern, r'<span class="wave">\1</span>', line["jp_raw"])
+                        elif type(line["jp_raw"]) == type(["list"]):
+                            for item in line["jp_raw"]:
+                                item = re.sub(unneeded_stuff_pattern, '', item)
+                                item = re.sub(shake_pattern, '', item)
+                                item = re.sub(wave_pattern, '', item)
+                write_json_to_file(path + "/" + file, file_lines)
+                bar()
+    
+    
+    
 generate_page_line_associations()
+format_line_associations()
